514dbe583de5d66be703ef48d64e490697e2a76f
Haigsov <haigsouv@my.yorku.ca>
2025-04-10 22:59:04 -0400

    Wrote some code that doesn't work

--- src/player/Player.cs
+++ src/player/Player.cs
@@ -3,4 +3,20 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+    public int speed = 200;
+
+    // Wrote some code that don't work
+    public override void _Ready()
+    {
+        // Called every time the node is added to the scene.
+        // Initialization here
+        Velocity = new Vector2(0, 0);
+    }
+    public override void _PhysicsProcess(double delta)
+    {
+        Velocity.Y -= new speed * delta;
+
+    }
+
+
 }
