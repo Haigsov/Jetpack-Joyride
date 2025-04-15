68812e6d95dc415fd4083c1a1f6e6f7886b8fea8
Haigsov <haigsouv@my.yorku.ca>
2025-04-14 23:23:57 -0400

    Figured out how Variables work

--- src/player/Player.cs
+++ src/player/Player.cs
@@ -5,14 +5,15 @@ using System.Numerics;
 public partial class Player : CharacterBody2D
 {
     public int speed = 200;
-
+    public Godot.Vector2 vel = Godot.Vector2.Zero;
     // Wrote some code that don't work
     public override void _Ready()
     {
         // Called every time the node is added to the scene.
         // Initialization here
-        Velocity = new Godot.Vector2(0, 0);
-        GD.Print("Player");
+        var velocity = Godot.Vector2.Zero;
+        GD.Print(velocity);
+        GD.Print(vel);
     }
 
 
