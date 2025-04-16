a1b0f67c9925b03e9104a3b54b0be8ac43aee286
Haigsov <haigsouv@my.yorku.ca>
2025-04-15 21:31:26 -0400

    Update Player.cs

--- src/player/Player.cs
+++ src/player/Player.cs
@@ -4,17 +4,22 @@ using System.Numerics;
 
 public partial class Player : CharacterBody2D
 {
-    public int speed = 200;
-    public Godot.Vector2 vel = Godot.Vector2.Zero;
+    public int gravity = 200;
+    public Godot.Vector2 velocity = Godot.Vector2.Zero;
     // Wrote some code that don't work
     public override void _Ready()
     {
         // Called every time the node is added to the scene.
         // Initialization here
-        var velocity = Godot.Vector2.Zero;
         GD.Print(velocity);
-        GD.Print(vel);
     }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        velocity = velocity * gravity;
+    }
+
 
 
 }
