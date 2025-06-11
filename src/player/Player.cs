5afcdbe2d123d763e2093b8c3a4bac5e72cf314c
Haigsov <haigsouv@my.yorku.ca>
2025-06-10 22:36:15 -0400

    Fixed player.cs compiler issues

--- src/player/Player.cs
+++ src/player/Player.cs
@@ -6,27 +6,30 @@ public partial class Player : CharacterBody2D
 {
     //public Godot.Vector2 Velocity;
     public int Gravity;
+    public Godot.Vector2 NewVelocity;
     public override void _Ready()
     {
         Velocity = Godot.Vector2.Zero;
-        Gravity = 10;
+        Gravity = 100;
+        NewVelocity = Velocity;
+
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        //Gravity making it go down
-        Set("Velocity") -= 10 * (float)delta;
-        player_controls(delta);
+        NewVelocity.Y += Gravity;
+        Velocity = NewVelocity;
+        // player_controls(delta);
         GD.Print(Velocity);
 
     }
 
-    public void player_controls(double delta)
-    {
-        if (Input.IsActionPressed("Jetpack")){
-            Velocity.Y = Velocity.Y + 100 * (float)delta;
-        }
-    }
+    // public void player_controls(double delta)
+    // {
+    //     if (Input.IsActionPressed("Jetpack")){
+    //         Velocity.Y = Velocity.Y + 100 * (float)delta;
+    //     }
+    // }
 
 
 
