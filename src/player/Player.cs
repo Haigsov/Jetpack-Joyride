5885d41bdb411143c1d3c7c2a589821cd677a932
Haigsov <haigsouv@my.yorku.ca>
2025-06-16 23:38:20 -0400

    Removed horizontal velocity
    I realized I might be able to make the game by making the level come to the player instead of the player go through the level.

--- src/player/Player.cs
+++ src/player/Player.cs
@@ -4,23 +4,22 @@ using System.Numerics;
 
 public partial class Player : CharacterBody2D
 {
-    //public Godot.Vector2 Velocity;
     public int Gravity;
     public Godot.Vector2 NewVelocity;
-    public int Speed;
+    // public int Speed;
 
     public override void _Ready()
     {
         Velocity = Godot.Vector2.Zero;
         Gravity = 200;
-        Speed = 200;
+        // Speed = 200;
         NewVelocity = Velocity;
     }
 
     public override void _PhysicsProcess(double delta)
     {
         NewVelocity.Y += Gravity * (float)delta;
-        NewVelocity.X += Speed * (float)delta;
+        // NewVelocity.X += Speed * (float)delta;
         PlayerJetpack(delta);
         Velocity = NewVelocity;
         MoveAndSlide();
