6fd756b1997977accc901e0712b1dbfc5c39f580
Haigsov <haigsouv@my.yorku.ca>
2025-06-18 21:29:07 -0400

    Initialized coin spawning
    And also improved code optimization and readability.

--- src/main/Main.cs
+++ src/main/Main.cs
@@ -4,18 +4,21 @@ using System.Runtime.InteropServices.Marshalling;
 
 public partial class Main : Node2D
 {
-	[Export]
-	public PackedScene CoinScene { get; set; }
+	// Loads coin scene in a variable to be instatiated later.
+	public PackedScene CoinScene = GD.Load<PackedScene>("res://src/coin/coin.tscn");
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 
-		Node coinInstance = CoinScene.Instantiate();
-
+		// Instatiate coin scene.
+		StaticBody2D coinInstance = (StaticBody2D)CoinScene.Instantiate();
+		coinInstance.Name = "Coin";
 		AddChild(coinInstance);
+		// Change posiiton of Coin after instantiating.
+		GetNode<StaticBody2D>("Coin").Position = new Vector2(321, 343);
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	// Not being used yet.
 	public override void _Process(double delta)
 	{
 	}
