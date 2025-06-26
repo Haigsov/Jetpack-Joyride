5d1df622acc40eb2a224597ce4e7b5e19918e5b6
Haigsov <haigsouv@my.yorku.ca>
2025-06-25 23:33:03 -0400

    Added 3 in main for now

--- src/main/Main.cs
+++ src/main/Main.cs
@@ -6,19 +6,25 @@ public partial class Main : Node2D
 {
 	// Loads coin scene in a variable to be instatiated later.
 	public PackedScene CoinScene = GD.Load<PackedScene>("res://src/coin/coin.tscn");
-	// public StaticBody2D Floor;
+	Coin coinInstance;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		// Floor = GetNode<StaticBody2D>("Floor");
 		// Instatiate coin scene.
-		Coin coinInstance = (Coin)CoinScene.Instantiate();
-		coinInstance.Name = "Coin";
-		AddChild(coinInstance);
+		for (int i = 0; i < 3; i++)
+		{
+			coinInstance = (Coin)CoinScene.Instantiate();
+			coinInstance.Name = $"Coin{i}";
+			// Change posiiton of Coin after instantiating.
+			//GetNode<Coin>($"Coin{i}")
+			coinInstance.Position = new Vector2(321 + (i * 100), 343);
+			AddChild(coinInstance);
+		}
 
-		// Change posiiton of Coin after instantiating.
-		GetNode<Coin>("Coin").Position = new Vector2(321, 343);
+
+
 	}
 
 	public override void _Process(double delta)
