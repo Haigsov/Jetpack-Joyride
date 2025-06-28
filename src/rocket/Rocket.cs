b9ec7ea0148d226ea08db09331ea780ef956b500
Haigsov <haigsouv@my.yorku.ca>
2025-06-28 13:21:28 -0400

    Created Rocket script

---
+++ src/rocket/Rocket.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Rocket : Area2D
+{
+
+	Vector2 velocity;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
