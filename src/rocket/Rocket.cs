2fc86eac0f56c25d64e9707b71df85f365542475
Haigsov <haigsouv@my.yorku.ca>
2025-06-30 23:44:31 -0400

    Made rocket move

--- src/rocket/Rocket.cs
+++ src/rocket/Rocket.cs
@@ -8,10 +8,19 @@ public partial class Rocket : Area2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		velocity = Vector2.Zero;
+		BodyEntered += OnBodyEntered;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		velocity = new Vector2(-40, 0);
+		Position += velocity * (float)delta;
+	}
+
+	public void OnBodyEntered(Node2D body)
+	{
+		body.QueueFree();
 	}
 }
