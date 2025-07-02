990ca9083d8609bfaab855826db476af801f40fe
Haigsov <haigsouv@my.yorku.ca>
2025-07-01 22:55:28 -0400

    Removed rocket

--- src/main/Main.cs
+++ src/main/Main.cs
@@ -11,14 +11,13 @@ public partial class Main : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// Floor = GetNode<StaticBody2D>("Floor");
 		// Instatiate coin scene.
 		for (int i = 0; i < 3; i++)
 		{
 			coinInstance = (Coin)CoinScene.Instantiate();
 			coinInstance.Name = $"Coin{i}";
+
 			// Change posiiton of Coin after instantiating.
-			//GetNode<Coin>($"Coin{i}")
 			coinInstance.Position = new Vector2(621 + (i * 100), 343);
 			AddChild(coinInstance);
 		}
