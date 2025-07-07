973fef138ea25de8b4192d2a114b47b936dead39
Haigsov <haigsouv@my.yorku.ca>
2025-07-06 23:31:45 -0400

    Polygon2D generates based on Collision2D shape

--- src/obstacle/Obstacle.cs
+++ src/obstacle/Obstacle.cs
@@ -4,17 +4,29 @@ using System;
 public partial class Obstacle : Area2D
 {
 	// CollisionShape2D Variable.
-	RectangleShape2D shape;
+	private RectangleShape2D _shape;
+	private Polygon2D poly;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		shape = GetNode<CollisionShape2D>("CollisionShape2D").Shape as RectangleShape2D;
-		GD.Print("Before: ", shape.Size);
+		_shape = GetNode<CollisionShape2D>("CollisionShape2D").Shape as RectangleShape2D;
+		GD.Print("Before: ", _shape.Size);
 
 		// Change CollisionShape2D's size.
-		shape.Size = new Vector2(400, 100);
-		GD.Print("After: ", shape.Size);
+		_shape.Size = new Vector2(400, 100);
+		GD.Print("After: ", _shape.Size);
+
+		poly = GetNode<Polygon2D>("Polygon2D");
+		Vector2 halfSize = _shape.Size / 2;
+
+		poly.Polygon = new Vector2[]
+		{
+			new Vector2(-halfSize.X,-halfSize.Y),
+			new Vector2(halfSize.X,-halfSize.Y),
+			new Vector2(halfSize.X,halfSize.Y),
+			new Vector2(-halfSize.X,halfSize.Y)
+		};
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
