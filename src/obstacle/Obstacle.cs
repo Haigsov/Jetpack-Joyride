cc69127eb388140fa1c8773096b0d5842467e5ad
Haigsov <haigsouv@my.yorku.ca>
2025-07-08 20:52:13 -0400

    Made Obstacle remove player from game when hit

--- src/obstacle/Obstacle.cs
+++ src/obstacle/Obstacle.cs
@@ -10,6 +10,8 @@ public partial class Obstacle : Area2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		BodyEntered += OnBodyEntered;
+
 		_shape = GetNode<CollisionShape2D>("CollisionShape2D").Shape as RectangleShape2D;
 		GD.Print("Before: ", _shape.Size);
 
@@ -34,5 +36,10 @@ public partial class Obstacle : Area2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void OnBodyEntered(Node2D body)
+	{
+		body.QueueFree();
 	}
 }
