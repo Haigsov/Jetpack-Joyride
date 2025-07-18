c8c3596197cf8cd0e56d3053aa7af66866f65109
Haigsov <haigsouv@my.yorku.ca>
2025-07-17 23:57:14 -0400

    Comments

--- src/obstacle/Obstacle.cs
+++ src/obstacle/Obstacle.cs
@@ -20,6 +20,7 @@ public partial class Obstacle : Area2D
 		_shape.Size = new Vector2(GD.RandRange(100, 450), 20);
 		GD.Print("After: ", _shape.Size);
 
+		// Change Polygon2D size.
 		poly = GetNode<Polygon2D>("Polygon2D");
 		Vector2 halfSize = _shape.Size / 2;
 
@@ -31,6 +32,7 @@ public partial class Obstacle : Area2D
 			new Vector2(-halfSize.X,halfSize.Y)
 		};
 
+		// Change Rotation.
 		Rotation = GD.RandRange(0, 360);
 
 		Position = new Vector2(GD.RandRange(100, 200), GD.RandRange(100, 200));
